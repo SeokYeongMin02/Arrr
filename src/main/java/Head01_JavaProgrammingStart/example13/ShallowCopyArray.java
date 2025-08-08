2def54003d1626b98055fc69785b190b9e68843b
석영민 <ttow778@gmail.com>
2025-08-08 16:42:43 +0900

    복사문

---
+++ src/main/java/Head01_JavaProgrammingStart/example13/ShallowCopyArray.java
@@ -0,0 +1,11 @@
+package Head01_JavaProgrammingStart.example13;
+
+public class ShallowCopyArray {
+    public static void main(String[] args) {
+        int[] original = {1, 2, 3};
+        int[] copy = original;
+        copy[0] = 100;
+        System.out.println(original[0]);
+        System.out.println(original[0]);
+    }
+}
